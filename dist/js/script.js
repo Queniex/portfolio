5f8973e7e993ad6f6e218f20a9121c3c239aa3d5
Queniex <87303828+Queniex@users.noreply.github.com>
2022-09-08 19:56:24 +0700

    Update automatic slider image

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,3 +18,55 @@ hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active')
     navMenu.classList.toggle('hidden')
 })
+
+//  Slider Image
+// let slideIndex = 1
+// showSlides(slideIndex)
+
+// function plusSlides(n){
+//     showSlides(slideIndex += n)
+// }
+
+// function currentSlide(n){
+//     showSlides(slideIndex = n)
+// }
+
+// function showSlides(n){
+//     let i
+//     let slides = document.getElementsByClassName('slides')
+//     let dots = document.getElementsByClassName('dot')
+//     if ( n > slides.length ){
+//         slideIndex = 1 
+//     }
+//     if ( n < 1 ){
+//         slideIndex = slides.length
+//     } 
+//     for ( i = 0; i < slides.length; i++ ){
+//         slides[i].style.display = "none";
+//     } 
+//     for ( i = 0; i < dots.length; i++ ) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex-1].style.display = "block";
+//     dots[slideIndex-1].className += " active";
+// }
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
